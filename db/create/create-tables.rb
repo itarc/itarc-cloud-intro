93480b45173947c59d1381b28f69b145aa0f1b5f
itarc <itarc@numericable.fr>
2014-03-25 14:03:39 +0100

    remove create-table.rb without s

--- db/create/create-tables.rb
+++ db/create/create-tables.rb
@@ -1,3 +1,6 @@
+require_relative '../Accesseur'
+db = Accesseur.new
+
 # ############
 nom_de_table = 'polls'; 
 colonnes = '
@@ -8,9 +11,6 @@ colonnes = '
 '
 # ############
 
-require_relative '../Accesseur'
-db = Accesseur.new
-
 puts "'Drop' de la table <#{nom_de_table}> si elle existe"
 db.execute_sql("drop table if exists #{nom_de_table}")
 
@@ -20,9 +20,6 @@ db.execute_sql("create table #{nom_de_table} (#{colonnes})")
 # ############
 nom_de_table = 'polls_save'; 
 # ############
-
-require_relative '../Accesseur'
-db = Accesseur.new
 
 puts "'Drop' de la table <#{nom_de_table}> si elle existe"
 db.execute_sql("drop table if exists #{nom_de_table}")
@@ -37,16 +34,13 @@ colonnes = '
 '
 # ############
 
-require_relative '../Accesseur'
-db = Accesseur.new
-
 puts "'Drop' de la table <#{nom_de_table}> si elle existe"
 db.execute_sql("drop table if exists #{nom_de_table}")
 
 puts "Creation de la table <#{nom_de_table}>"
 db.execute_sql("create table #{nom_de_table} (#{colonnes})")
 
-puts "Initailisation du compteur de table <#{nom_de_table}> avec 0"
+puts "Initialisation du compteur de table <#{nom_de_table}> avec 0"
 db.execute_sql("insert into #{nom_de_table} values (0)")
 
 # ############
@@ -54,9 +48,7 @@ nom_de_table = 'teacher_current_slide';
 colonnes = '
   current_slide_id TEXT
 '
-
-require_relative '../Accesseur'
-db = Accesseur.new
+# ############
 
 puts "'Drop' de la table <#{nom_de_table}> si elle existe"
 db.execute_sql("drop table if exists #{nom_de_table}")
@@ -64,5 +56,33 @@ db.execute_sql("drop table if exists #{nom_de_table}")
 puts "Creation de la table <#{nom_de_table}>"
 db.execute_sql("create table #{nom_de_table} (#{colonnes})")
 
-puts "Initailisation du compteur de table <#{nom_de_table}> avec 1"
-db.execute_sql("insert into #{nom_de_table} values (1)")
+puts "Initialisation numero de slide courant <#{nom_de_table}> avec 0"
+db.execute_sql("insert into #{nom_de_table} values (0)")
+
+# ############
+nom_de_table = 'run_events'; 
+colonnes = '
+  timestamp TEXT,
+  user_id TEXT,
+  type TEXT,
+  slide_index TEXT,
+  code_input TEXT,
+  code_output TEXT
+'
+# ############
+
+puts "'Drop' de la table <#{nom_de_table}> si elle existe"
+db.execute_sql("drop table if exists #{nom_de_table}")
+
+puts "Creation de la table <#{nom_de_table}>"
+db.execute_sql("create table #{nom_de_table} (#{colonnes})")
+
+# ############
+nom_de_table = 'run_events_save'; 
+# ############
+
+puts "'Drop' de la table <#{nom_de_table}> si elle existe"
+db.execute_sql("drop table if exists #{nom_de_table}")
+
+puts "Creation de la table <#{nom_de_table}>"
+db.execute_sql("create table #{nom_de_table} (#{colonnes})")
